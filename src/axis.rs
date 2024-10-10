6818b76582f3ee947358b7ae9983b8a73d7991c6
Jan Pipek <jan.pipek@gmail.com>
2024-10-10 16:39:32 +0200

    find_bin + test

--- src/axis.rs
+++ src/axis.rs
@@ -9,6 +9,14 @@ impl AxisData {
         self.bin_edges.len() - 1
     }
 
+    pub fn min(&self) -> f64 {
+        self.bin_edges[0]
+    }
+
+    pub fn max(&self) -> f64 {
+        *self.bin_edges.last().unwrap()
+    }
+
     pub fn new(bin_edges: Vec<f64>) -> Self {
         AxisData {
             bin_edges,
@@ -22,7 +30,50 @@ impl AxisData {
     pub fn get_bin(&self, n: usize) -> (f64, f64) {
         (self.bin_edges[n], self.bin_edges[n+1])
     }
+
+    pub fn apply(&self, data: &[f64]) -> Vec<f64> {
+        let mut result = vec![0.0; self.len()];
+        for value in data.iter() {
+            if let Some(bin) = self.find_bin(*value) {
+                result[bin] += 1.0;
+            }
+        }
+        result
+    }
+
+    pub fn find_bin(&self, value: f64) -> Option<usize> {
+        if value < self.min() {
+            return None;
+        }
+        for (i, edge) in self.bin_edges.iter().skip(1).enumerate() {
+            if *edge > value {
+                return Some(i);
+            }
+        }
+        if value == self.max() {
+            return Some(self.len() - 1);
+        }
+        None
+    }
 }
 
-
 // TODO: Actually have this as trait so that we can find the contents fast
+
+#[cfg(test)]
+mod tests {
+    mod find_bin {
+        use crate::axis::AxisData;
+
+        #[test]
+        fn test_below() {
+            let ax = AxisData { bin_edges: vec![1.0, 2.0, 3.0]};
+
+            assert_eq!(ax.find_bin(1.0), Some(0));
+            assert_eq!(ax.find_bin(2.2), Some(1));
+            assert_eq!(ax.find_bin(3.0), Some(1));
+
+            assert_eq!(ax.find_bin(0.2), None);
+            assert_eq!(ax.find_bin(3.2), None);
+        }
+    }
+}
